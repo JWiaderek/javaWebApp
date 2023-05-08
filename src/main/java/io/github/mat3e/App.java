6d5977eca17edeb36ad909958822dd5ba28b9739
Julek <juliush6wiaderek@gmail.com>
2023-05-08 17:01:05 +0200

    commit

---
+++ src/main/java/io/github/mat3e/App.java
@@ -0,0 +1,13 @@
+package io.github.mat3e;
+
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
+
+public class App {
+    public static void main(String[] args) {
+        Logger logger = (Logger) LoggerFactory.getLogger(App.class);
+        logger.info("Hello world");
+    }
+}
---
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -0,0 +1,16 @@
+package io.github.mat3e;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "hello", urlPatterns = {"/api/*"})
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello world");
+    }
+}
